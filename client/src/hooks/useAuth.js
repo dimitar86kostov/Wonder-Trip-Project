a892f4131510be61824e845b779930fd87fa65b6
dimitar86kostov <145497494+dimitar86kostov@users.noreply.github.com>
2024-07-26 11:19:13 +0300

    add useAuth custom hook

---
+++ client/src/hooks/useAuth.js
@@ -0,0 +1,33 @@
+import { useContext } from "react";
+import { login, register } from "../api/auth-api";
+import AuthContext from "../contexts/AuthContext";
+
+export function useLogin() {
+    const { changeAuthState } = useContext(AuthContext)
+
+    const loginHandler = async (email, password) => {
+
+        const { password: _, ...authData  } = await login(email, password);
+
+        changeAuthState(authData);
+
+        return authData;
+    }
+
+    return loginHandler
+}
+
+export function useRegister() {
+    const { changeAuthState } = useContext(AuthContext)
+
+    const registerHandler = async (email, password) => {
+
+        const { password: _, ...authData  } = await register(email, password);
+
+        changeAuthState(authData);
+
+        return authData;
+    }
+
+    return registerHandler
+}
