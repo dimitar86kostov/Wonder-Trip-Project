cba9d145f3cd3ecb111faa5842c911185ca6052b
dimitar86kostov <145497494+dimitar86kostov@users.noreply.github.com>
2024-08-18 15:42:45 +0300

    some updates

--- client/src/components/tripsList/TripsList.jsx
+++ client/src/components/tripsList/TripsList.jsx
@@ -35,21 +35,3 @@ export default function TripsList() {
     </section>
   );
 }
-
-// const CONTENTS = [
-//   {
-//     img: "https://www.material-tailwind.com/image/product-4.png",
-//     name: "Linen Suit",
-//     price: "$2,500"
-//   },
-//   {
-//     img: "https://www.material-tailwind.com/image/product-3.png",
-//     name: "Tweed Suit",
-//     price: "$2,300"
-//   },
-//   {
-//     img: "https://www.material-tailwind.com/image/product-5.png",
-//     name: "Premium Suit",
-//     price: "$1,240"
-//   },
-// ];
