cba50fa874db67ee3501f3b035a7bc900d82b541
dimitar86kostov <145497494+dimitar86kostov@users.noreply.github.com>
2025-05-29 19:43:57 +0300

    adding icon + No Reult text on dismatch search

--- client/src/components/tripsList/TripsList.jsx
+++ client/src/components/tripsList/TripsList.jsx
@@ -7,7 +7,7 @@ import { useGetAllTrips } from "../../hooks/useTrips";
 
 export default function TripsList() {
   const [trips, setTrips] = useState([]);
-  const [allTrips, , isFetching] = useGetAllTrips(); // оригиналните
+  const [allTrips, , isFetching] = useGetAllTrips();
   const [searchLoading, setSearchLoading] = useState(false);
 
   useEffect(() => {
@@ -45,11 +45,40 @@ export default function TripsList() {
       </div>
 
       <div className="mx-auto container">
-        <div className="grid grid-cols-1 gap-8 lg:grid-cols-3 md:grid-cols-2">
+        {/* <div className="grid grid-cols-1 gap-8 lg:grid-cols-3 md:grid-cols-2">
           {Array.isArray(trips) && trips.map((trip) => (
             <TripListItem key={trip._id} {...trip} />
           ))}
+        </div> */}
+        <div className="grid grid-cols-1 gap-8 lg:grid-cols-3 md:grid-cols-2">
+          {searchLoading || isFetching ? (
+            <Spinner className="h-16 w-16 items-center text-gray-900/50 mx-auto" />
+          ) : trips.length > 0 ? (
+            trips.map((trip) => (
+              <TripListItem key={trip._id} {...trip} />
+            ))
+          ) : (
+            <p className="col-span-full text-center text-gray-600 text-lg flex flex-col items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-20 h-20 mb-2 text-gray-400"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M10 10c1.657 0 3-1.343 3-3S11.657 4 10 4 7 5.343 7 7s1.343 3 3 3zM21 21l-4.35-4.35M10 14c-3.314 0-6-2.686-6-6s2.686-6 6-6 6 2.686 6 6c0 1.362-.456 2.614-1.224 3.618M9 16h2v2H9v-2z"
+                />
+              </svg>
+              Oops! We couldn’t find any trips matching your search.
+            </p>
+
+          )}
         </div>
+
       </div>
     </section>
   );
